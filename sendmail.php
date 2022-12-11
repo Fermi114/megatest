1b9bf36752dada6ac830102da6ea16169751a769
Fermi114 <119268398+Fermi114@users.noreply.github.com>
2022-12-11 15:01:36 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -7,7 +7,7 @@
 
     $mail = new PHPMailer(true);
     $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->setLanguage('ru');
     $mail->IsHTML(true);
 
     $mail->setFrom('info@op.gr', 'Помощник ТесПО');
@@ -16,15 +16,10 @@
 
     $body = '<h1>Мега письмо!</h1>';
 
-    if(trim(!empty($_POST['name']))){
-        $body.='<p><strong>Имя </strong> '.$_POST['name'].'</p>';
-    }
-    if(trim(!empty($_POST['email']))){
-        $body.='<p><strong>Почта</strong> '.$_POST['email'].'</p>';
-    }
-    if(trim(!empty($_POST['message']))){
-        $body.='<p><strong>Сообщение</strong> '.$_POST['message'].'</p>';
-    }
+    /*if(trim(!empty($_POST['name']))){*/
+    $body.='<p><strong>Имя </strong> '.$_POST['name'].'</p>';
+    $body.='<p><strong>Почта</strong> '.$_POST['email'].'</p>';
+    $body.='<p><strong>Сообщение</strong> '.$_POST['message'].'</p>';
 
     $mail->Body = $body;
 
